d250a155e3af3be2f5dab871eaf214b94fd968a1
Mauro Allegrini <mauro.allegrini@gmail.com>
2020-09-03 13:11:20 +0200

    OOP Version

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,28 +1,19 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
+class GildedRose:
 
     def __init__(self, items):
-        self.items = items
+        self.orig_items = items
+        self.items = [WrappedItem.factory(item) for item in items]
 
     def update_quality(self):
-        for item in self.items:
-            old_quality, old_sell_in = item.quality, item.sell_in
-            item.quality -= 1
-            item.sell_in -= 1
-            if old_sell_in <= 0 or item.name == 'Conjured': item.quality -= 1
-            if item.quality < 0: item.quality = 0
-            if item.name == 'Aged Brie': item.quality = old_quality + 1
-            if item.quality > 50: item.quality = 50
-            if item.name == "Sulfuras, Hand of Ragnaros": item.quality = old_quality
-            if item.name == 'Backstage passes to a TAFKAL80ETC concert':
-                item.quality = old_quality + 1
-                if item.sell_in <= 10: item.quality += 1
-                if item.sell_in <= 5:  item.quality += 1
-                if item.sell_in <= 0:  item.quality = 0
+        for i, item in enumerate(self.items):
+            item.update()
+            self.orig_items[i] = item
 
 
 class Item:
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
@@ -30,3 +21,55 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class WrappedItem(Item):
+
+    @staticmethod
+    def factory(item):
+        if item.name == "Aged Brie": return AgedBrie(item.name, item.sell_in, item.quality)
+        if item.name == "Sulfuras, Hand of Ragnaros": return Sulfuras(item.name, item.sell_in, item.quality)
+        if item.name == "Backstage passes to a TAFKAL80ETC concert": return BackstagePass(item.name, item.sell_in, item.quality)
+        if item.name == "Conjured": return Conjured(item.name, item.sell_in, item.quality)
+        return WrappedItem(item.name, item.sell_in, item.quality)
+
+    def validate(self):
+        if self.quality < 0: self.quality = 0
+        if self.quality > 50: self.quality = 50
+    
+    def update(self):
+        self.sell_in -= 1
+        self.quality -= 1
+        if self.sell_in < 0: self.quality -= 1
+        self.validate()
+        
+class Sulfuras(WrappedItem):
+    
+    def update(self):
+        self.sell_in -= 1
+        
+
+class AgedBrie(WrappedItem):
+
+    def update(self):
+        self.quality += 1
+        self.sell_in -= 1
+        self.validate()
+
+
+class BackstagePass(WrappedItem):
+    
+    def update(self):
+        self.sell_in -= 1
+        self.quality += 1
+        if self.sell_in <= 10: self.quality += 1
+        if self.sell_in <= 5:  self.quality += 1
+        if self.sell_in <= 0:  self.quality = 0
+        self.validate()
+
+
+class Conjured(WrappedItem):
+    
+    def update(self):
+        self.sell_in -= 1
+        self.quality -= 2
+        self.validate()
